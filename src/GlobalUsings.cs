38532c7f89cf566010f7500f11b13a161e1ef0c2
Blake Pell <blakepell@hotmail.com>
2025-05-29 07:28:35 -0400

    Global usings, Argus.Core Nuget Reference

---
+++ src/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Checkers
+ *
+ * @project lead      : Blake Pell
+ * @company           : ApexGate
+ * @website           : https://www.apexgate.net
+ * @website           : https://www.blakepell.com
+ * @copyright         : Copyright (c), 2023-2025 All rights reserved.
+ * @license           : MIT
+ */
+
+using Argus.Extensions;
